a46172cd99820407d65cbbe44d2eafc86b84b085
gadjodilo26 <contact@rms-dev.fr>
2025-07-18 21:19:32 +0200

    18/07/2025

--- data/events.js
+++ data/events.js
@@ -3,8 +3,19 @@
  * Date au format YYYY-MM-DD, titre à afficher dans le calendrier.
  */
 const MEDICAL_EVENTS = [
+  // Myélogramme
   { date: "2025-07-15", title: "Myélogramme" },
-  
-  // <-- Ajoute ici tes nouveaux événements, un par ligne :
-  // { date: "2025-09-01", title: "Consultation oncologue" },
+
+  // Rendez-vous médicaux
+  { date: "2025-07-21", title: "Rendez-vous Clinique Louis Pasteur à 10h00" },
+  { date: "2025-07-28", title: "Rendez-vous Clinique Louis Pasteur à 10h00" },
+  { date: "2025-08-01", title: "Rendez-vous Dr Clément à 15h30" },
+  { date: "2025-08-04", title: "Rendez-vous Clinique Louis Pasteur à 10h30" },
+  { date: "2025-08-11", title: "Rendez-vous Clinique Louis Pasteur à 13h30" },
+
+  // Injections en ambulatoire (immunothérapie)
+  { date: "2025-07-21", title: "Injections Daratumumab SC + Velcade SC" },
+  { date: "2025-07-28", title: "Injections Daratumumab SC + Velcade SC" },
+  { date: "2025-08-04", title: "Injections Daratumumab SC + Velcade SC" },
+  { date: "2025-08-11", title: "Injections Daratumumab SC + Velcade SC" },
 ];
